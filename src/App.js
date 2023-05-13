4336e962d476e3b4f3275771274c320bf141248b
Roshan Guragain <twistfreaky@gmail.com>
2023-05-13 15:15:56 +0545

    initial setup of component structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react'
 
+import List from './components/List/List'
+import Header from './components/Header/Header'
+import Map from './components/Map/Map'
+
+
+
 const App = () => {
   return (
     <div>
-        <h1>Hello world</h1>
+        <Header/>
+        <List/>
+        <Map/>
+
     </div>
   )
 }
