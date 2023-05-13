7f82ff631cfb8dd0ac62e1f76753963ae8de3ef1
Roshan Guragain <twistfreaky@gmail.com>
2023-05-14 00:17:47 +0545

    component added : List & PlaceDetails

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,8 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './List.scss'
+import PlaceDetails from '../PlaceDetails/PlaceDetails'
 
 const List = () => {
+  const [type, setType] = useState('restaurants');
+  const [rating, setRating] = useState('')
+
+  // dummy data
+
+  const places = [
+    { name: 'CCool place' },
+    { name: 'best place' },
+    { name: 'CCool place' },
+    { name: 'best place' },
+    { name: 'CCool place' },
+    { name: 'best place' },
+    { name: 'CCool place' },
+    { name: 'best place' },
+    { name: 'CCool place' },
+    { name: 'best place' }
+  ]
+
   return (
-    <div>List</div>
+    <div className='container'>
+      <h4>Restaurants $ Hotels near you</h4>
+
+      <div className="formsControl">
+      <div class="formControl">
+        <label for="type">Type</label>
+        <select value={type} onchange={(e) => setRating(e.target.value)}>
+          <option value="restaurants">Restaurants</option>
+          <option value="hotels">Hotels</option>
+          <option value="attractions">Attractions</option>
+        </select>
+      </div>
+
+      <div class="formControl">
+        <label for="rating">Rating</label>
+        <select value={rating} onchange={(e) => setRating(e.target.value)}>
+          <option value={0}>All</option>
+          <option value={3}>Above 3.0</option>
+          <option value={4}>Above 4.0</option>
+          <option value={4.5}>Above 4.5</option>
+        </select>
+      </div>
+      </div>
+
+      <div className="list">
+        {places.map((place, i) => (
+          <div key={i}>
+            <PlaceDetails place={place}/>
+          </div>
+        ))}
+      </div>
+
+    </div>
   )
 }
 
