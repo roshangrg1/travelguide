293513e8bb578276c80d4ca3fdfed5b2e06ca959
Roshan Guragain <twistfreaky@gmail.com>
2023-05-16 09:53:19 +0545

    made dynamic from static data

--- src/api/index.js
+++ src/api/index.js
@@ -1,24 +1,23 @@
 import axios from "axios";
 
 const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary'
-const options = {
-  params: {
-    bl_latitude: '11.847676',
-    tr_latitude: '12.838442',
-    bl_longitude: '109.095887',
-    tr_longitude: '109.149359',
-   
-  },
-  headers: {
-    'X-RapidAPI-Key': '1c82fa12c4mshbbc6083ecb60092p1f677fjsn3e1c796e9399',
-    'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
-  }
-};
 
 
-const getPlaceData = async () =>{
+ export const getPlacesData = async (sw , ne) =>{
     try {
-        const {data: {data}}= await axios.get()
+        const {data: {data}}= await axios.get(URL ,{
+          params: {
+            bl_latitude: sw.lat,
+            tr_latitude: ne.lat,
+            bl_longitude: sw.lng,
+            tr_longitude: ne.lng,
+           
+          },
+          headers: {
+            'X-RapidAPI-Key': '1c82fa12c4mshbbc6083ecb60092p1f677fjsn3e1c796e9399',
+            'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
+          }
+        })
         return data
     } catch (error) {
         console.log(error.message)
