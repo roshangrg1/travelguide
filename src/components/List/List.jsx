261f82f6088f024f1e242861ba13485d3a77429b
Roshan Guragain <twistfreaky@gmail.com>
2023-05-16 12:49:16 +0545

    setType corrected

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -26,30 +26,30 @@ const List = () => {
       <h4>Restaurants $ Hotels near you</h4>
 
       <div className="formsControl">
-      <div class="formControl">
-        <label for="type">Type</label>
-        <select value={type} onchange={(e) => setRating(e.target.value)}>
-          <option value="restaurants">Restaurants</option>
-          <option value="hotels">Hotels</option>
-          <option value="attractions">Attractions</option>
-        </select>
-      </div>
+        <div className="formControl">
+          <label for="type">Type</label>
+          <select value={type} onChange={(e) => setType(e.target.value)}>
+            <option value="restaurants">Restaurants</option>
+            <option value="hotels">Hotels</option>
+            <option value="attractions">Attractions</option>
+          </select>
+        </div>
 
-      <div class="formControl">
-        <label for="rating">Rating</label>
-        <select value={rating} onchange={(e) => setRating(e.target.value)}>
-          <option value={0}>All</option>
-          <option value={3}>Above 3.0</option>
-          <option value={4}>Above 4.0</option>
-          <option value={4.5}>Above 4.5</option>
-        </select>
-      </div>
+        <div className="formControl">
+          <label for="rating">Rating</label>
+          <select value={rating} onChange={(e) => setRating(e.target.value)}>
+            <option value={0}>All</option>
+            <option value={3}>Above 3.0</option>
+            <option value={4}>Above 4.0</option>
+            <option value={4.5}>Above 4.5</option>
+          </select>
+        </div>
       </div>
 
       <div className="list">
-        {places.map((place, i) => (
+        {places?.map((place, i) => (
           <div key={i}>
-            <PlaceDetails place={place}/>
+            <PlaceDetails place={place} />
           </div>
         ))}
       </div>
